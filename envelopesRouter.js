90d9858ecaa38e08063fb7a67a73dfac0484a9d6
Ahmad Soliman <ahmad.s.soliman@gmail.com>
2023-11-24 00:55:59 +0200

    added all functions

--- envelopesRouter.js
+++ envelopesRouter.js
@@ -5,12 +5,17 @@ const envelopesRouter = express.Router();
 let envelopes = [
     {
         id: 0,
-        budget: 0,
-        title: "envelope 1"
+        total_budget: 1500,
+        title: "total budget"
     },
     {
         id: 1,
-        budget: 0,
+        budget: 500,
+        title: "envelope 1"
+    },
+    {
+        id: 2,
+        budget: 500,
         title: "envelope 2"
     }
 ]
@@ -35,9 +40,60 @@ envelopesRouter.get('/:id', (req, res) => {
     const idToFind = req.params.id;
 
     const foundEnvelope = envelopes.find((envelope) => envelope.id === parseInt(idToFind));
-        
     res.send(foundEnvelope);
     
+});
+
+envelopesRouter.put('/:id/subtract/:value', (req, res) => {
+    const idToFind = req.params.id;
+    const foundEnvelope = envelopes.find((envelope) => envelope.id === parseInt(idToFind));
+    if(req.params.value > foundEnvelope.budget) {
+        res.send('value is bigger than the envelope budget');
+    } else{
+        //subtract value from envelope:
+        foundEnvelope.budget -= req.params.value;
+        //subtract value from total budget:
+        envelopes[0].total_budget -= req.params.value;
+        //send message to the use.
+        res.send(`You have withdrawn ${req.params.value} from ${foundEnvelope.title}. The remaining is ${foundEnvelope.budget} and total remaining budget is ${envelopes[0].total_budget}`)
+    }
+});
+
+envelopesRouter.delete('/:id', (req, res) => {
+    const idToFind = req.params.id;
+    const foundEnvelope = envelopes.find((envelope) => envelope.id === parseInt(idToFind));
+    if (!foundEnvelope) {
+        res.send('wrong ID.');
+    } 
+
+    const indexToDelete = envelopes.findIndex((item) => item.id === parseInt(idToFind));
+    envelopes.splice(indexToDelete, 1);
+    //console.log(indexToDelete);
+    res.send(envelopes);
+    //const updatedEnvelopes = envelopes.filter(e => e.id !== parseInt(foundEnvelope.id));
+    //res.send(updatedEnvelopes);
+
+});
+
+envelopesRouter.post('/transfer/:from/:to', (req, res) => {
+    const fromId = req.params.from;
+    const toId = req.params.to;
+    const amount = req.body.amount;
+    const foundEnvelopeFrom = envelopes.find((envelope) => envelope.id === parseInt(fromId));
+    if (!foundEnvelopeFrom) {
+        res.send('wrong from ID.');
+    } 
+
+    const foundEnvelopeTo = envelopes.find((envelope) => envelope.id === parseInt(toId));
+    if (!foundEnvelopeTo) {
+        res.send('wrong to ID.');
+    } 
+
+    foundEnvelopeFrom.budget -= amount;
+    foundEnvelopeTo.budget += amount;
+
+    res.send(`you have transfered the amount of ${amount} from ${foundEnvelopeFrom.title} and is now ${foundEnvelopeFrom.budget} to ${foundEnvelopeTo.title} and is now ${foundEnvelopeTo.budget}. `)
+
 });
 
 module.exports = envelopesRouter;
